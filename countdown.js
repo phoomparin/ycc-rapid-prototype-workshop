3c118d49c4b98eeea6c4e7705256547b745304b5
Glitch (hello-webpage) <none>
2020-05-06 12:33:04 +0000

    🎆🎁 Checkpoint ./countdown.css:4013284/136:14035597/222:15892368/151:15892773/241:15893868/1 ./countdown.html:4013284/40:14035597/57:15892368/74:15892773/18:15893868/601 ./countdown.js:4013284/970 ./vote.html:14035597/6255 ./index.html:15893011/104 ./index-page.css:15893011/1138 ./style.css:4001215/6

--- countdown.js
+++ countdown.js
@@ -1,1 +1,16 @@
 console.log("hi");
+
+window.addEventListener('DOMContentLoaded', () => {
+  let currentTime = document.querySelector('#current-time')
+  
+  let timer = setInterval(() => {
+    const now = new Date()
+    
+    let pad = num => num < 10 ? '0' + num : num
+    let HH = pad(now.getHours())
+    let MM = pad(now.getMinutes())
+    let SS = pad(now.getSeconds())
+  
+    currentTime.textContent = `${HH}:${MM}:${SS}`
+  }, 1000)
+})
