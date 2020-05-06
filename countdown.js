f97adf41969b655c5e9e6dbd7387de83b4ad2ab7
Glitch (ycc-rapid-prototype-workshop) <none>
2020-05-06 13:34:05 +0000

    🍡🐲 Checkpoint ./countdown.js:577339/2294:4013284/1706 ./vote.css:14035597/1267:15892368/84 ./countdown.html:577339/503:4013284/4:15893868/109 ./vote.html:14035597/489

--- countdown.js
+++ countdown.js
@@ -1,6 +1,6 @@
 /* global Vue, vm, firebase */
 
-let pad = num => (num < 10 ? "0" + num : num);
+const pad = n => String(n).padStart(2, 0)
 
 function formatHMS(now) {
   let HH = pad(now.getHours());
@@ -10,15 +10,63 @@ function formatHMS(now) {
   return `${HH}:${MM}:${SS}`;
 }
 
+function getRemaining(diff) {
+  diff /= 1000
+  
+  let HH = Math.floor(diff / 3600)
+  diff = diff - HH * 3600
+  
+  let MM = Math.floor(diff / 60)
+  diff = diff - MM * 60
+  
+  let SS = Math.floor(diff)
+  
+  return `${pad(HH)}:${pad(MM)}:${pad(SS)}`
+}
+
+
 vm = new Vue({
   el: "#app",
   data: {
-    currentTime: "...",
-    targetTime: "..."
+    currentTime: new Date(),
+    targetTime: new Date(),
+    logs: {},
+    remainingTime: ''
   },
   methods: {
     format(time) {
       return formatHMS(time)
+    },
+    setTime() {
+      const answer = prompt('Enter finish time', this.format(this.targetTime))
+      if (!answer) {
+        return
+      }
+      const [h = 0, m = 0, s = 0] = answer.split(':').map(x => +x || 0)
+      const targetTime = new Date()
+      targetTime.setHours(h)
+      targetTime.setMinutes(m)
+      targetTime.setSeconds(s)
+      targetTime.setMilliseconds(0)
+      if (targetTime < Date.now()) {
+        targetTime.setTime(targetTime.getTime() + 24 * 60 * 60 * 1000)
+      }
+      firebase.database().ref('countdown/target').set(targetTime.toJSON())
+      firebase.database().ref('countdown/log').push({
+        time: new Date().toJSON(),
+        message: 'Set target time to ' + this.format(targetTime)
+      })
+    },
+    extendTime() {
+      
+    },
+    markAsFinished() {
+      
+    },
+    updateRemainingTime() {
+      let diff = this.targetTime - this.currentTime
+      
+      this.remainingTime = getRemaining(diff)
     }
   }
 });
@@ -27,10 +75,15 @@ firebase.database().ref('countdown/target').on('value', (snapshot) => {
   vm.targetTime = new Date(snapshot.val())
 })
 
+firebase.database().ref('countdown/log').on('value', (snapshot) => {
+  vm.logs = snapshot.val()
+})
+
 window.addEventListener("DOMContentLoaded", () => {
   let timer = setInterval(() => {
     const now = new Date();
 
     vm.currentTime = new Date();
+    vm.updateRemainingTime();
   }, 1000);
 });
