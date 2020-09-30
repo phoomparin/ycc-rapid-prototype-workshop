50bb1b26e33b5e32e4c762e3502945ab9d36b489
Chayanon Thongpila <nutchy.th@gmail.com>
2020-09-30 21:20:14 +0700

    :sparkles: Add CSS flash animation

--- countdown.js
+++ countdown.js
@@ -1,28 +1,28 @@
 /* global Vue, vm, firebase */
 
-const pad = n => String(n).padStart(2, 0)
+const pad = (n) => String(n).padStart(2, 0)
 
 function formatHMS(now) {
-  let HH = pad(now.getHours());
-  let MM = pad(now.getMinutes());
-  let SS = pad(now.getSeconds());
-  
-  return `${HH}:${MM}:${SS}`;
+  let HH = pad(now.getHours())
+  let MM = pad(now.getMinutes())
+  let SS = pad(now.getSeconds())
+
+  return `${HH}:${MM}:${SS}`
 }
 
 function getRemaining(diff) {
   diff /= 1000
-  
+
   let HH = Math.floor(diff / 3600)
   diff = diff - HH * 3600
-  
+
   let MM = Math.floor(diff / 60)
   diff = diff - MM * 60
-  
+
   let SS = Math.floor(diff)
-  
+
   if (HH < 0) return '00:00:00'
-  
+
   return `${pad(HH)}:${pad(MM)}:${pad(SS)}`
 }
 
@@ -30,14 +30,13 @@ function reverseOrder(obj) {
   return Object.values(obj).sort((a, b) => new Date(b.time) - new Date(a.time))
 }
 
-
 vm = new Vue({
-  el: "#app",
+  el: '#app',
   data: {
     currentTime: new Date(),
     targetTime: new Date(),
     logs: {},
-    remainingTime: '00:00:00'
+    remainingTime: '00:00:00',
   },
   methods: {
     format(time) {
@@ -48,7 +47,7 @@ vm = new Vue({
       if (!answer) {
         return
       }
-      const [h = 0, m = 0, s = 0] = answer.split(':').map(x => +x || 0)
+      const [h = 0, m = 0, s = 0] = answer.split(':').map((x) => +x || 0)
       const targetTime = new Date()
       targetTime.setHours(h)
       targetTime.setMinutes(m)
@@ -58,52 +57,72 @@ vm = new Vue({
         targetTime.setTime(targetTime.getTime() + 24 * 60 * 60 * 1000)
       }
       firebase.database().ref('countdown/target').set(targetTime.toJSON())
-      firebase.database().ref('countdown/log').push({
-        time: new Date().toJSON(),
-        message: 'Set target time to ' + this.format(targetTime)
-      })
+      firebase
+        .database()
+        .ref('countdown/log')
+        .push({
+          time: new Date().toJSON(),
+          message: 'Set target time to ' + this.format(targetTime),
+        })
     },
     extendTime() {
       const answer = prompt('How many minutes to extend?', 5)
       if (!answer) return
-      
+
       let minutes = Math.max(Number(answer), 1)
-      
-      const targetTime = new Date(this.targetTime.valueOf() + (1000 * 60 * minutes))
+
+      const targetTime = new Date(
+        this.targetTime.valueOf() + 1000 * 60 * minutes
+      )
 
       firebase.database().ref('countdown/target').set(targetTime.toJSON())
-      firebase.database().ref('countdown/log').push({
-        time: new Date().toJSON(),
-        message: `Extend by ${minutes} minutes`
-      })
+      firebase
+        .database()
+        .ref('countdown/log')
+        .push({
+          time: new Date().toJSON(),
+          message: `Extend by ${minutes} minutes`,
+        })
     },
     markAsFinished() {
       firebase.database().ref('countdown/log').push({
         time: new Date().toJSON(),
-        message: `Marked this session as finished.`
+        message: `Marked this session as finished.`,
       })
     },
     updateRemainingTime() {
       let diff = this.targetTime - this.currentTime
-      
+
       this.remainingTime = getRemaining(diff)
-    }
-  }
-});
-
-firebase.database().ref('countdown/target').on('value', (snapshot) => {
-  vm.targetTime = new Date(snapshot.val())
+    },
+  },
+  computed: {
+    flashTimer() {
+      return false
+    },
+  },
 })
 
-firebase.database().ref('countdown/log').orderByChild('time').on('value', (snapshot) => {
-  vm.logs = reverseOrder(snapshot.val())
-})
+firebase
+  .database()
+  .ref('countdown/target')
+  .on('value', (snapshot) => {
+    vm.targetTime = new Date(snapshot.val())
+  })
 
-window.addEventListener("DOMContentLoaded", () => {
+firebase
+  .database()
+  .ref('countdown/log')
+  .orderByChild('time')
+  .on('value', (snapshot) => {
+    vm.logs = reverseOrder(snapshot.val())
+  })
+
+window.addEventListener('DOMContentLoaded', () => {
   let timer = setInterval(() => {
-    const now = new Date();
+    const now = new Date()
 
-    vm.currentTime = new Date();
-    vm.updateRemainingTime();
-  }, 1000);
-});
+    vm.currentTime = new Date()
+    vm.updateRemainingTime()
+  }, 1000)
+})
